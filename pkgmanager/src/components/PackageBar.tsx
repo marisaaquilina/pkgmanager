006590ca6e17a94d96a17b0554e649c9699cf0d8
marisaaquilina <marisa.aquilina@gmail.com>
2019-07-31 16:33:37 -0700

    Working PackageBar

--- pkgmanager/src/components/PackageBar.tsx
+++ pkgmanager/src/components/PackageBar.tsx
@@ -1,7 +1,36 @@
-import * as React from 'react';
 
-export function PackageSearcher(props: any) {
+import { Kernel } from '@jupyterlab/services';
+
+import React, { useState } from 'react';
+
+function kernel(input: string): void {
+  Kernel.listRunning().then(kernelModels => {
+    console.log(kernelModels, kernelModels[0])
+    const kernel = Kernel.connectTo(kernelModels[0]);
+    if (kernel) {
+      kernel.requestExecute({ code: '%pip install ' + input});
+      console.log("Finished '%pip install " + input)
+    }
+  });
+}
+
+export function PackageSearcher() {
+  // Declare a new state variable, which we'll call "count"
+  const [input, setInput] = useState('');
   return (
-    <div>SHOW</div>
+    <div>
+      <p>You clicked {input} times</p>
+      <input
+        value={input}
+        onChange={e => setInput(e.target.value)}
+        placeholder="First name"
+        type="text"
+        name="firstName"
+        required
+      />
+      <button onClick={() => kernel(input)}>
+        Click me
+      </button>
+    </div>
   );
 }
--- pkgmanager/src/index.ts
+++ pkgmanager/src/index.ts
@@ -6,7 +6,6 @@ import {
   INotebookTools, INotebookTracker
 } from '@jupyterlab/notebook';
 
-import { Kernel } from '@jupyterlab/services';
 
 import PackageTool from './components/PackageTool';
 
@@ -18,15 +17,8 @@ const extension: JupyterFrontEndPlugin<void> = {
   autoStart: true,
   requires: [INotebookTools, INotebookTracker],
   activate: (app: JupyterFrontEnd, cellTools: INotebookTools, notebookTracker: INotebookTracker) => {
-    console.log('2');
-    Kernel.listRunning().then(kernelModels => {
-      console.log(kernelModels, kernelModels[0])
-      const kernel = Kernel.connectTo(kernelModels[0]);
-      if (kernel) {
-        kernel.requestExecute({ code: '%pip install gender-guesser' });
-        console.log("Finished")
-      }
-  });
+    console.log(8);
+
   const packageTool = new PackageTool(app, notebookTracker);
   cellTools.addItem({ tool: packageTool });
   }
